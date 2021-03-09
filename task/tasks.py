5c267fd4c238c102b72d6042d3cd6990b5537185
Paritosh Mishra <emailaritoshmishra@gmail.com>
2021-03-09 11:26:53 +0530

    changes

--- task/tasks.py
+++ task/tasks.py
@@ -1,9 +1,19 @@
 from celery.task import task
+from django.conf import settings
+import requests
 import random
 
 @task(bind=True, max_retries=3)
-def update_status(self):
+def fetch_conversion(self):
+    # 3rd party api having irregular response or intermittent failures 
+    # here for example currency conversion rates are fetched by using fixer api
     try:
-        # 3rd party api having irregular response or intermittent failures 
-    except Error as exc:
+        endpoint = 'latest'
+        access_key = settings.CURRENCY_API_KEY
+        base_currency = "$"
+        url = 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&base=' + base_currency
+        response = requests.post(url=url)
+        response_json = response.json()
+        currency = response_json["rates"]
+    except Exception as exc:
         self.retry(exc=exc, countdown=int(random.uniform(2, 4) ** self.request.retries))
